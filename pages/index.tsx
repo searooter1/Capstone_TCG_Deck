af2de73a70995e2a06a3296c29360a47ed54f12d
searooter1 <deruitercole@gmail.com>
2026-02-05 23:32:50 -0500

    Start basic homepage

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <h1 className="-mt-12 text-2xl">Home</h1>
+        </div>
+    )
+}
