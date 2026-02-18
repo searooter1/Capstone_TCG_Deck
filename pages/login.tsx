6dc42ec4a272f907a39668b0d5304bc818a1707e
searooter1 <deruitercole@gmail.com>
2026-02-18 14:07:20 -0500

    Simple login, fix not needed authbuttons component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,7 @@
-import AuthButtons from "@/components/Auth"
 import { signIn, signOut, useSession } from "next-auth/react"
-import {useRouter} from "next/router";
-import {useEffect} from "react";
+import Image from "next/image";
 
-export default function TestAuth() {
+export default function Login() {
     const result = useSession()
 
     const session = result.data
@@ -11,7 +9,7 @@ export default function TestAuth() {
 
     return (
         <main className="flex flex-col items-center justify-center min-h-screen gap-2">
-            <div>Status: {status}</div>
+            <div>{status}</div>
 
             {!session ? (
                 <button className={"inline-flex gap-1 bg-blue-500 rounded-md px-8 pr-6 py-2 mt-1 md:text-lg text-sm font-medium text-gray-300 transition hover:bg-blue-400 hover:text-white"}
@@ -20,6 +18,15 @@ export default function TestAuth() {
                 </button>
             ) : (
                 <div>
+                    {session.user?.image && (
+                        <Image
+                            src={session.user.image}
+                            alt="Avatar"
+                            width={32}
+                            height={32}
+                            className="rounded-full"
+                        />
+                    )}
                     <p>Username: <b>{session.user?.name}</b></p>
                     <p>Email: <b>{session.user?.email}</b></p>
                     <button className={"inline-flex gap-1 bg-blue-500 rounded-md px-8 pr-6 py-2 mt-1 md:text-lg text-sm font-medium text-gray-300 transition hover:bg-blue-400 hover:text-white"}
